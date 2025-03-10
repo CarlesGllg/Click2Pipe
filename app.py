3b27da7c1b4b916b7e8fe614c9e674ec373d777c
CarlesGllg <cgallego@robotix.es>
2025-03-10 13:33:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -123,8 +123,7 @@ def create_PD_follow_up_activity(api_key, org_id, user_id, activity_text, activi
         'type': 'follow_up',  # Type of activity
         'org_id': org_id,  # Organization ID (make sure this is valid)
         'user_id': user_id,  # User ID assigned to this activity (make sure this is valid)
-        'note': activity_text,  # The body of the activity (text)
-        'type_name': 'SEGUIMIENTO'
+        'note': activity_text  # The body of the activity (text)
     }
     
     # Make a POST request to create the activity
