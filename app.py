67b9338a45c7f847babcb75aebf2cb44f418d3a1
CarlesGllg <cgallego@robotix.es>
2025-04-08 13:30:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def listen_to_webhook():
         print(f"DADES_TASK: {comments}")
         custom_fields = task_data.get('custom_fields', [])
     
-        print("CUSTOM: ", custom_fields)
+        #print("CUSTOM: ", custom_fields)
     
         extracted_value = None
 
@@ -85,7 +85,7 @@ def listen_to_webhook():
         pdoid = get_PD_organization_info ('61283559c8d298f5a3fc1eece05d7c2b1e5617c3', extracted_value)
     
         if (pdoid is not None):
-            print(f"PD-OID: {pdoid}")
+            #print(f"PD-OID: {pdoid}")
             com_user = 5371109   #Usuari de l'Adriana
             pd_org_id = extracted_value
             activity = create_PD_follow_up_activity('61283559c8d298f5a3fc1eece05d7c2b1e5617c3', pd_org_id, com_user, com_text, com_data)
